463ce9045c4564b403454ae07714ffdbb7d45594
Shinnosuke <kagoshin@gmail.com>
2017-07-06 05:14:19 +0000

    continued practice

--- shellScript/for2.sh
+++ shellScript/for2.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
-<<comment
-for i in {1..10}
-do
-	echo " loop No: $i."
-done
-
 
 for i in {1..10}
 do
-  echo "loop No: $i"
-done
-
-
-
-echo
-comment
-for ((i=0 ; i<10 ; i++))
-do
-  echo "loop2 No: $i"
+	echo " $i 回目のループです."
 done
