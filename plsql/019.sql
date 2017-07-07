79a445e1dd15759181dbf1944529eb548c54d8cd
Shinnosuke Kagoshima <kagoshin@gmail.com>
2017-07-07 15:47:28 +0900

    【plsql】変数、例外処理

---
+++ plsql/019.sql
@@ -0,0 +1,14 @@
+DECLARE
+  dname DEPT.DNAME%Type;
+BEGIN
+  select DNAME INTO dname
+  from DEPT
+  where DEPTNO = 99;
+
+  SYS.DBMS_OUTPUT.PUT_LINE('DEPTNO=99のDNAME=' || dname);
+
+EXCEPTION
+  WHEN no_data_found THEN
+    SYS.DBMS_OUTPUT.PUT_LINE('例外が発生しました。SQLCODE=' || SQLCODE ||
+    '、エラーメッセージ＝' || SQLERRM);
+END;
