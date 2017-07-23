69f09caa76d5ddd0c8b8f4a97820397997ed3986
Shinnosuke Kagoshima <kagoshin@gmail.com>
2017-07-24 00:34:43 +0900

    export exam

---
+++ plsql/exportData/dept_export.sql
@@ -0,0 +1,9 @@
+--------------------------------------------------------
+--  ファイルを作成しました - 月曜日-7月-24-2017   
+--------------------------------------------------------
+REM INSERTING into SYSTEM.DEPT
+SET DEFINE OFF;
+Insert into SYSTEM.DEPT (DEPTNO,DNAME,LOC) values (10,'ACCOUNTING','NEW YORK');
+Insert into SYSTEM.DEPT (DEPTNO,DNAME,LOC) values (20,'RESEARCH','DALLAS');
+Insert into SYSTEM.DEPT (DEPTNO,DNAME,LOC) values (30,'SALES','CHICAGO');
+Insert into SYSTEM.DEPT (DEPTNO,DNAME,LOC) values (40,'OPERATIONS','BOSTON');
