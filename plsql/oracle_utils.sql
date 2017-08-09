b9ab7a2fd49e683aabd6fe9642c88756129ef2f9
Shinnosuke Kagoshima <kagoshin@gmail.com>
2017-08-10 01:47:11 +0900

    ディレクトリオブジェクト

---
+++ plsql/oracle_utils.sql
@@ -0,0 +1,7 @@
+SET SERVEROUTPUT ON
+
+SELECT * FROM user_sys_privs;
+
+SELECT * FROM ALL_DIRECTORIES;
+
+select * from all_tables;
---
+++ plsql/practice002/08-15.sql
@@ -0,0 +1,5 @@
+CREATE OR REPLACE DIRECTORY utl_data
+AS '/Users/kagoshin/tutorial/plsql/dirobj';
+
+CREATE OR REPLACE DIRECTORY UTL_DATA_2
+AS '/u01/dirobj';
