7f6be090de7cec916f2a0884da6ceb68c8b052f0
Shinnosuke Kagoshima <kagoshin@gmail.com>
2017-11-02 01:11:47 +0900

    【SpringBootDemo】

--- workspace/SpringBootDemo-2/src/main/java/com/example/demo/domain/Authorities.java
+++ workspace/SpringBootDemo-2/src/main/java/com/example/demo/domain/Authorities.java
@@ -1,12 +1,43 @@
 package com.example.demo.domain;
 
-import org.springframework.security.core.GrantedAuthority;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "authorities")
 public class Authorities {
 
-    public GrantedAuthority getId() {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+    @Id
+    private AuthoritiesId id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private User user;
+
+    public Authorities() {
     }
 
+    public Authorities(AuthoritiesId id) {
+        this.id = id;
+    }
+
+    public AuthoritiesId getId() {
+        return id;
+    }
+
+    public void setId(AuthoritiesId id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
---
+++ workspace/SpringBootDemo-2/src/main/java/com/example/demo/domain/AuthoritiesId.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain;
+
+public class AuthoritiesId {
+
+    public String getAuthority() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+}
