9bee6032ab0507ef42e90335ce811d73eda3a1e3
Shinnosuke Kagoshima <kagoshin@gmail.com>
2017-11-04 22:15:31 +0900

    【pl/sql】プロシージャ 11/4

--- plsql/practice002/06−09.sql
+++ plsql/practice002/06−09.sql
@@ -1,14 +1,18 @@
 CREATE OR REPLACE PROCEDURE debug_test
 IS
-  sal_col emp.sal%TYPE;
-  comm_col emp.comm%TYPE;
-  total_col emp.total%TYPE;
+  sal_col   emp.sal%TYPE;
+  comm_col  emp.comm%TYPE;
+  total_col emp.sal%TYPE;
 BEGIN
-  select sal, comm INTO sal_col, comm_col
+  select sal, NVL(comm, 0) INTO sal_col, comm_col
   from EMP
   where empno = 7369;
 
+  -- DBMS_OUTPUT.PUT_LINE('sal_col: ' || sal_col);
+  -- DBMS_OUTPUT.PUT_LINE('comm_col: ' || comm_col);
+
   total_col := sal_col + comm_col;
+  -- DBMS_OUTPUT.PUT_LINE('total_col: ' || total_col);
   DBMS_OUTPUT.PUT_LINE(total_col);
 END;
 /
