14e23e9a454ae207798229575f9f58b3137c4f63
Shinnosuke Kagoshima <kagoshin@gmail.com>
2018-06-06 00:06:50 +0900

    【sql】ループ3

---
+++ sql/practicalIntro/ch5/004.sql
@@ -0,0 +1,36 @@
+--リスト5.5 郵便番号テーブルの定義
+create table PostalCode (
+  pcode char(7),
+  district_name varchar2(256),
+  constraint pk_pcode primary key(pcode)
+)
+;
+
+INSERT INTO PostalCode VALUES ('4130001',  '静岡県熱海市泉');
+INSERT INTO PostalCode VALUES ('4130002',  '静岡県熱海市伊豆山');
+INSERT INTO PostalCode VALUES ('4130103',  '静岡県熱海市網代');
+INSERT INTO PostalCode VALUES ('4130041',  '静岡県熱海市青葉町');
+INSERT INTO PostalCode VALUES ('4103213',  '静岡県伊豆市青羽根');
+INSERT INTO PostalCode VALUES ('4380824',  '静岡県磐田市赤池');
+
+--commit;
+
+--リスト5.6 郵便番号のランキングを求めるクエリ
+--set autotrace on explain
+select pcode
+       , district_name
+       , case when pcode = '413033' then 0
+              when pcode like '413003%' then 1
+              when pcode like '41300%'  then 2
+              when pcode like '4130%'   then 3
+              when pcode like '413%'    then 4
+              when pcode like '41%'     then 5
+              when pcode like '4%'      then 6
+              else null end as rank
+from PostalCode
+;
+
+
+--リスト5.7 最寄の郵便番号を求めるクエリ
+
+--リスト5.8 ウィンドウ関数による解
