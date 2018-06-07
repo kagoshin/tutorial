58c1fefbd16e02c10e4b34cb8cafa2a79eb63c01
Shinnosuke Kagoshima <kagoshin@gmail.com>
2018-06-08 00:01:09 +0900

    【sql】ループ4

--- sql/practicalIntro/ch5/004.sql
+++ sql/practicalIntro/ch5/004.sql
@@ -32,5 +32,61 @@ from PostalCode
 
 
 --リスト5.7 最寄の郵便番号を求めるクエリ
+--set autotrace on explain
+select pcode
+       , district_name
+from PostalCode
+where case when pcode = '4130033' then 0
+           when pcode like '413003%' then 1
+           when pcode like '41300%'  then 2
+           when pcode like '4130%'   then 3
+           when pcode like '413%'    then 4
+           when pcode like '41%'     then 5
+           when pcode like '4%'      then 6
+           else null end =
+            (select min(case when pcode = '4130033' then 0
+                             when pcode like '413003%' then 1
+                             when pcode like '41300%'  then 2
+                             when pcode like '4130%'   then 3
+                             when pcode like '413%'    then 4
+                             when pcode like '41%'     then 5
+                             when pcode like '4%'      then 6
+                             else null end)
+             from PostalCode);
 
 --リスト5.8 ウィンドウ関数による解
+--set autotrace on explain
+select pcode
+       , district_name
+from (
+  select pcode
+         , district_name
+         , case when pcode = '4130033' then 0
+                when pcode like '413003%' then 1
+                when pcode like '41300%'  then 2
+                when pcode like '4130%'   then 3
+                when pcode like '413%'    then 4
+                when pcode like '41%'     then 5
+                when pcode like '4%'      then 6
+                else null end as hit_code
+         , min(case when pcode = '4130033' then 0
+                    when pcode like '413003%' then 1
+                    when pcode like '41300%'  then 2
+                    when pcode like '4130%'   then 3
+                    when pcode like '413%'    then 4
+                    when pcode like '41%'     then 5
+                    when pcode like '4%'      then 6
+                    else null end)
+           over ( order by case when pcode = '4130033' then 0
+                                when pcode like '413003%' then 1
+                                when pcode like '41300%'  then 2
+                                when pcode like '4130%'   then 3
+                                when pcode like '413%'    then 4
+                                when pcode like '41%'     then 5
+                                when pcode like '4%'      then 6
+                                else null end
+           ) as min_code
+    from POSTALCODE
+) Foo
+where hit_code = min_code
+;
