019c62ad14460ccef8f70dff22204784913b1f47
Shinnosuke Kagoshima <kagoshin@gmail.com>
2018-08-20 22:51:57 +0900

    【sql】ナンバリング4

--- sql/practicalIntro/ch8/006.sql
+++ sql/practicalIntro/ch8/006.sql
@@ -48,3 +48,61 @@ select num
          as next_num
 from numbers
 ;
+
+--リスト8.19 シーケンスを求める（集合指向的）
+select min(num) as low
+       , '〜'
+       , max(num) as high
+from (
+  select n1.num
+         , count(n2.num) - n1.num as gp
+  from numbers n1
+  join numbers n2
+  on n2.num <= n1.num
+  group by n1.num
+) n
+group by gp
+;
+
+select n1.num
+       --, count(n2.num) - n1.num as gp
+        , count(n2.num)
+       --, n2.num
+from numbers n1
+join numbers n2
+on n2.num <= n1.num
+group by n1.num
+order by n1.num
+;
+
+--リスト8.20 シーケンスを求める（手続き型）
+select low
+        , high
+from (
+  select low
+         , case when high is null
+              then min(high) over (order by seq
+                                   rows between current row
+                                        and unbounded following)
+              else high end as high
+  from (
+    select case when coalesce(prev_diff, 0) <> 1
+                then num else null end as low
+           , case when coalesce (next_diff, 0) <> 1
+                  then num else null end as high
+           , seq
+    from (
+      select num
+             , max(num) over (order by num
+                              rows between 1 following
+                                   and 1 following) - num as next_diff
+             , num - max(num) over (order by num
+                                    rows between 1 preceding
+                                         and 1 preceding) as prev_diff
+             , row_number() over (order by num) as seq
+      from numbers
+    ) tmp1
+  ) tmp2
+) tmp3
+where low is not null
+;
