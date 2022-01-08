80f17ee568b15da2b356006e02c5b24ed96702e5
kagoshin <kagoshin@gmail.com>
2022-01-09 00:56:05 +0900

    [C_Projects]『1週間でC#の基礎が学べる本』2日目

---
+++ C_Projects/Sample202/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sample202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b = 3;
+            int add, sub;
+            double avg;
+            a = 6;
+            add = a + b;
+            sub = a - b;
+            avg = (a + b) / 2.0;
+            Console.WriteLine("{0} + {1} = {2}", a, b, add);
+            Console.WriteLine("{0} - {1} = {2}", a, b, sub);
+            Console.WriteLine("{0}と{1}の平均値{2}", a, b, avg);
+        }
+    }
+}
---
+++ C_Projects/Sample204/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sample204
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, d;
+            int c;
+
+            a = 1.23;
+            b = (double)12;
+            c = (int)1.24;
+            d = 10;
+
+            Console.WriteLine("a={0} b={1} c={2} d={3}",
+                a, b, c, d);
+        }
+    }
+}
