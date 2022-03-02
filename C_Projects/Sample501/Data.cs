625d96c340b1a3af9557aa21c2c0053a8a5a7302
kagoshin <kagoshin@gmail.com>
2022-03-03 00:52:17 +0900

    [C_Projects]『1週間でC#の基礎が学べる本』5-1

---
+++ C_Projects/Sample501/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Sample501
+{
+    public class Data
+    {
+        private static int num = 0;
+        private int id;
+
+        public Data(int id)
+        {
+            this.id = id;
+            num++;
+            Console.WriteLine("値：{0} 数：{1}", id, num);
+        }
+
+        public static void ShowNumber()
+        {
+            Console.WriteLine("Dataオブジェクトの数：{0}", num);
+        }
+    }
+}
---
+++ C_Projects/Sample501/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sample501
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data[] d = new Data[2];
+            Data.ShowNumber();
+            for(int i = 0; i < d.Length; i++)
+            {
+                d[i] = new Data(i * 100);
+                Data.ShowNumber();
+            }
+        }
+    }
+}
