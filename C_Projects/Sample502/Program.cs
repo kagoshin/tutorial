a8ccdd65acf9d9c07c60c7a493d69029dff45092
kagoshin <kagoshin@gmail.com>
2022-03-04 01:38:35 +0900

    [C_Projects]『1週間でC#の基礎が学べる本』5-2

---
+++ C_Projects/Sample502/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sample502
+{
+    class Program
+    {
+        private static int snum = 100;
+        public int inum = 200;
+
+        public static void Foo()
+        {
+            Console.WriteLine("fooメソッド（staticメソッド）");
+        }
+
+        public void bar()
+        {
+            Console.WriteLine("barメソッド（インスタンスメソッド）");
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("pのインスタンスフィールド：inum = {0}", p.inum);
+            Console.WriteLine("Programのクラスフィールド：snum = {0}", snum);
+            Foo();
+            p.bar();
+        }
+    }
+}
