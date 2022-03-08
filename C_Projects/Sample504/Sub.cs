f5cea8a77c4465a2acc5018d8b08bfc0308fdd8f
kagoshin <kagoshin@gmail.com>
2022-03-09 00:38:39 +0900

    [C_Projects]『1週間でC#の基礎が学べる本』5-4

---
+++ C_Projects/Sample504/Sub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Sample504
+{
+    public class Sub : Super
+    {
+        public Sub()
+        {
+            Console.WriteLine("Subクラスのコンストラクタ（引数なし）");
+        }
+
+        public Sub(int param) : base(param)
+        {
+            Console.WriteLine("Subクラスのコンストラクタ（引数：param = {0}）", param);
+        }
+
+        ~Sub()
+        {
+            Console.WriteLine("Subクラスのデストラクタ");
+        }
+    }
+}
