efbc83f5b4c7df0fb4a16d8d7a1e9ad3628f8e8f
kagoshin <kagoshin@gmail.com>
2022-03-11 00:41:33 +0900

    [C_Projects]『1週間でC#の基礎が学べる本』5-6

---
+++ C_Projects/Sample509/Vector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Sample509
+{
+    class Vector : VectorBase
+    {
+        private double x = 0.0;
+        private double y = 0.0;
+
+        public override double X
+        {
+            set { x = value; }
+            get { return x; }
+        }
+
+        public override double Y
+        {
+            set { y = value; }
+            get { return y; }
+        }
+    }
+}
---
+++ C_Projects/Sample509/VectorBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Sample509
+{
+    abstract class VectorBase
+    {
+        public abstract double X
+        {
+            set;
+            get;
+        }
+
+        public abstract double Y
+        {
+            set;
+            get;
+        }
+    }
+}
