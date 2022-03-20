d919a7e5f79da26c84461e9e70dc53ac1d722039
kagoshin <kagoshin@gmail.com>
2022-03-21 01:42:33 +0900

    [C_Projects]『1週間でC#の基礎が学べる本』6-4

---
+++ C_Projects/Sample605/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sample605
+{
+    delegate void Operation(int a, int b);
+
+    class Calc
+    {
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+        }
+    }
+
+    class Program
+    {
+        static void Add(int a, int b)
+        {
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+        }
+
+        static void Main(string[] args)
+        {
+            Calc c = new Calc();
+
+            Operation o1 = new Operation(Add);
+            Operation o2 = new Operation(c.Sub);
+
+            o1(2, 1);
+            o2(2, 1);
+        }
+    }
+}
---
+++ C_Projects/Sample606/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Sample606
+{
+    class Program
+    {
+        delegate void Action(int a);
+
+        static void Func1(int a)
+        {
+            Console.WriteLine("a={0}", a);
+        }
+
+        static void Func2(int a)
+        {
+            Console.WriteLine("a*2={0}", a * 2);
+        }
+
+        static void Func3(int a)
+        {
+            Console.WriteLine("a*3={0}", a * 3);
+        }
+
+        static void Main(string[] args)
+        {
+            Action a = new Action(Func1);
+            a += new Action(Func2);
+            a += new Action(Func3);
+            a(3);
+        }
+    }
+}
