288415ad9a134fef8bca1c27123611b8b430750c
kagoshin <kagoshin@gmail.com>
2022-03-24 00:26:27 +0900

    [C_Projects]『1週間でC#の基礎が学べる本』6-12

--- C_Projects/Sample608/Program.cs
+++ C_Projects/Sample608/Program.cs
@@ -4,9 +4,26 @@ namespace Sample608
 {
     class Program
     {
+        static int GetNum(int i)
+        {
+            int[] nums = { 300, 600, 900 };
+            if (i > nums.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return nums[i];
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                int result = GetNum(4);
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("配列の範囲外にアクセスしました");
+            }
         }
     }
 }
