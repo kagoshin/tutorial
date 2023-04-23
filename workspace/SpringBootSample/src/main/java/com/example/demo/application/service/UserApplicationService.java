494d275d3d8996bf665caf08edf3032b5b855526
kagoshin <kagoshin@gmail.com>
2023-04-24 01:37:38 +0900

    [SpringBootSample]6-1-1（タブをスペースに変換）

--- workspace/SpringBootSample/src/main/java/com/example/demo/application/service/UserApplicationService.java
+++ workspace/SpringBootSample/src/main/java/com/example/demo/application/service/UserApplicationService.java
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserApplicationService {
 
-	public Map<String, Integer> getGenderMap() {
-		
-		Map<String, Integer> genderMap = new LinkedHashMap<>();
-		genderMap.put("男性", 1);
-		genderMap.put("女性", 2);
-		return genderMap;
-	}
+    public Map<String, Integer> getGenderMap() {
+        
+        Map<String, Integer> genderMap = new LinkedHashMap<>();
+        genderMap.put("男性", 1);
+        genderMap.put("女性", 2);
+        return genderMap;
+    }
 }
