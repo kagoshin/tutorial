bca76361673226a7fe4c166e9f30ce921961ce4c
kagoshin <kagoshin@gmail.com>
2023-04-25 11:00:13 +0900

    [SpringBootSample]8-2-2

--- workspace/SpringBootSample/src/main/java/com/example/demo/domain/user/service/UserService.java
+++ workspace/SpringBootSample/src/main/java/com/example/demo/domain/user/service/UserService.java
@@ -1,8 +1,12 @@
 package com.example.demo.domain.user.service;
 
+import java.util.List;
+
 import com.example.demo.domain.user.model.MUser;
 
 public interface UserService {
 
     public void signup(MUser user);
+
+    public List<MUser> getUsers();
 }
--- workspace/SpringBootSample/src/main/java/com/example/demo/domain/user/service/impl/UserServiceImpl.java
+++ workspace/SpringBootSample/src/main/java/com/example/demo/domain/user/service/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.demo.domain.user.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +20,10 @@ public class UserServiceImpl implements UserService {
         user.setDepartmentId(1);
         user.setRole("ROLE_GENERAL");
         mapper.insertOne(user);
+    }
+    
+    @Override
+    public List<MUser> getUsers() {
+        return mapper.findMany();
     }
 }
