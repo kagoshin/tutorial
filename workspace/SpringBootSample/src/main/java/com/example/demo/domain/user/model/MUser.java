b4bd4094243a1876c59a0e2c900c165ddd2b7d5f
kagoshin <kagoshin@gmail.com>
2023-05-01 17:12:50 +0900

    [SpringBootSample]13-3-3

--- workspace/SpringBootSample/src/main/java/com/example/demo/domain/user/model/MUser.java
+++ workspace/SpringBootSample/src/main/java/com/example/demo/domain/user/model/MUser.java
@@ -5,14 +5,16 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name="m_user")
+@Table(name = "m_user")
 public class MUser {
     @Id
     private String userId;
@@ -23,8 +25,12 @@ public class MUser {
     private Integer gender;
     private Integer departmentId;
     private String role;
-    @Transient
+
+    @ManyToOne(optional = true)
+    @JoinColumn(insertable = false, updatable = false, name = "departmentId")
     private Department department;
-    @Transient
+    
+    @OneToMany
+    @JoinColumn(insertable = false, updatable = false, name = "userId")
     private List<Salary> salaryList;
 }
