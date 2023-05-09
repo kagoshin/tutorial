07aeb087c32782038f9e57c9cc9da049186e4d76
kagoshin <kagoshin@gmail.com>
2023-05-09 18:06:42 +0900

    [ReactNextJs]3-1-3

--- javaScript/ReactNextJs/ch03/react-sample/src/index_copy.tsx
+++ javaScript/ReactNextJs/ch03/react-sample/src/index_copy.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import Hello from './components/Hello'
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -10,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Hello />
+    <App />
   </React.StrictMode>
 );
 
