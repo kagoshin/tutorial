8e931783684b88eb0b754817ce1b75aacc5bb6bf
kagoshin <kagoshin@gmail.com>
2023-05-09 23:53:19 +0900

    [ReactNextJs]3-4

--- javaScript/ReactNextJs/ch03/react-sample/src/index.tsx
+++ javaScript/ReactNextJs/ch03/react-sample/src/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Hello from './components/Hello'
+// import Hello from './components/Hello'
+// import Name from './components/Name'
+// import Message from './components/Message'
+// import ContainerSample from './components/ContainerSample'
+import ContextSample from './components/ContextSample'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -10,7 +14,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Hello />
+    {/* <Hello /> */}
+    {/* <Name /> */}
+    {/* <Message /> */}
+    {/* <ContainerSample /> */}
+    <ContextSample />
   </React.StrictMode>
 );
 
