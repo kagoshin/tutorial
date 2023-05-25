667d87db7d44f21f6375d5e22c693a6d62d106bd
kagoshin <kagoshin@gmail.com>
2023-05-25 15:27:43 +0900

    [ReactNextJs]4-3-4②

---
+++ javaScript/ReactNextJs/ch04/next-sample_02/stories/StyledButton6.stories.tsx
@@ -0,0 +1,32 @@
+import { ComponentMeta, Story } from '@storybook/react'
+import { StyledButton, StyledButtonProps } from '../components/StyledButton'
+import { linkTo } from '@storybook/addon-links'
+
+export default {
+  title: 'StyledButton (6) - Linkを使用する',
+  component: StyledButton,
+} as ComponentMeta<typeof StyledButton>
+
+export const Primary: Story<StyledButtonProps> = (props) => {
+  return (
+    <StyledButton {...props} variant="primary" onClick={linkTo('StyledButton (6) - Linkを使用する', 'Success')}>
+      Primary
+    </StyledButton>
+  )
+}
+
+export const Success: Story<StyledButtonProps> = (props) => {
+  return (
+    <StyledButton {...props} variant="success" onClick={linkTo('StyledButton (6) - Linkを使用する', 'Transparent')}>
+      Primary
+    </StyledButton>
+  )
+}
+
+export const Transparent: Story<StyledButtonProps> = (props) => {
+  return (
+    <StyledButton {...props} variant="transparent" onClick={linkTo('StyledButton (6) - Linkを使用する', 'Primary')}>
+      Transparent
+    </StyledButton>
+  )
+}
