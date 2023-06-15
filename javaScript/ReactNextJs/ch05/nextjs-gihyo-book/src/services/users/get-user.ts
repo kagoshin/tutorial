e71192a534f0a86f5d10fdb355ae7541d1cba051
kagoshin <kagoshin@gmail.com>
2023-06-15 14:14:39 +0900

    [ReactNextJs]6-2①（コンパイルエラー修正）

--- javaScript/ReactNextJs/ch05/nextjs-gihyo-book/src/services/users/get-user.ts
+++ javaScript/ReactNextJs/ch05/nextjs-gihyo-book/src/services/users/get-user.ts
@@ -1,4 +1,5 @@
-import type { ApiContext, User } from 'types'
+//import { ApiContext, User } from 'types'
+import type { ApiContext, User } from '@/types/data'
 import { fetcher } from 'utils'
 
 export type GetUserParams = {
